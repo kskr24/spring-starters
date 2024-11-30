6c404dc8d9ac90bece5bd1de5790178d1943af79
kskr24 <sk7193958@gmail.com>
2024-11-30 12:34:49 +0530

    Use @Bean and @Component annotation together to let the SpringContext add beans

--- src/main/java/beans/Person.java
+++ src/main/java/beans/Person.java
@@ -1,9 +1,16 @@
 package beans;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Person {
-    private String name;
+    private String name = "Ella";
     private Parrot parrot;
 
+    public Person(@Qualifier("parrot2")Parrot p){
+        this.parrot = p;
+    }
     public void setName(String name){
         this.name = name;
     }
--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -3,9 +3,11 @@ package config;
 import beans.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "beans")
 public class ProjectConfig {
 
     @Bean
@@ -19,16 +21,6 @@ public class ProjectConfig {
     public Parrot parrot2(){
         var p = new Parrot();
         p.setName("Miki");
-        return p;
-    }
-
-    @Bean
-    public Person person(
-            @Qualifier("parrot1")Parrot parrot
-    ){
-        Person p = new Person();
-        p.setName("Ella");
-        p.setParrot(parrot);
         return p;
     }
 
