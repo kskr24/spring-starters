eb8e220a3486e1a708412f75c7fc38c9859e92dc
kskr24 <sk7193958@gmail.com>
2024-12-01 04:28:32 +0530

    Use @Component to allow spring create the classes necessary

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,19 @@
 package org.example;
 
+import config.ProjectConfiguration;
 import model.Comment;
-import proxies.EmailCommentNotificationProxy;
-import repositories.DBCommentRepository;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
-        var commentService = new CommentService(commentRepository, commentNotificationProxy);
-        var comment = new Comment();
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
+        var comment = new Comment();
         comment.setAuthor("Carlo Ancelotti");
         comment.setText("We'll be in the final in Munich.");
 
+        var commentService = context.getBean(CommentService.class);
         commentService.publishComment(comment);
     }
 }
